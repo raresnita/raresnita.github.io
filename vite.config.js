fc2815c58e29a3fa2980b2a78ce275e0a7550849
raresnita <rares.nita2001@gmail.com>
2026-02-12 09:42:38 +0200

    update: remove node_modules and dist

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({ plugins: [tailwindcss()] });
+export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				ro: resolve(__dirname, "ro.html"),
+			},
+		},
+	},
+	plugins: [tailwindcss()],
+});
